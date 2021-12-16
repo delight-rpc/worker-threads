0c32a27d8c3caa385cc5b1e571a75ecfcdfbe95b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-16 16:53:27 +0000

    chore: upgrade dependencies
    BREAKING CHANGE:
    - The minimum version is Node.js v16

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { pathsToModuleNameMapper } = require('ts-jest')
 const { compilerOptions } = require('./tsconfig.base.json')
 
 module.exports = {
   preset: 'ts-jest'
-, testEnvironment: 'node'
+, testEnvironment: 'jsdom'
 , testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)']
 , moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/'

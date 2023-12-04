21e1006c6389331a55babe3f23b6c23661637e1a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-12-04 03:02:38 +0000

    test: add cases

--- __tests__/main-as-client-and-worker-as-server/index.spec.ts
+++ __tests__/main-as-client-and-worker-as-server/index.spec.ts
@@ -1,9 +1,10 @@
-import { createClient } from '@src/client.js'
+import { createBatchClient, createClient } from '@src/client.js'
 import { Worker } from 'worker_threads'
 import { IAPI } from './contract.js'
 import path from 'path'
 import { getErrorPromise } from 'return-style'
 import { fileURLToPath } from 'url'
+import { createBatchProxy } from 'delight-rpc'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -25,12 +26,36 @@ describe('Main as Client, Worker as Server', () => {
     expect(result).toBe('hello')
   })
 
+  test('echo (batch)', async () => {
+    const [client, close] = createBatchClient(worker)
+    const proxy = createBatchProxy<IAPI>()
+
+    const result = await client.parallel(proxy.echo('hello'))
+    close()
+
+    expect(result.length).toBe(1)
+    expect(result[0].unwrap()).toBe('hello')
+  })
+
   test('error', async () => {
     const [client, close] = createClient<IAPI>(worker)
 
     const err = await getErrorPromise(client.error('hello'))
     close()
 
+    expect(err).toBeInstanceOf(Error)
+    expect(err!.message).toMatch('hello')
+  })
+
+  test('error (batch)', async () => {
+    const [client, close] = createBatchClient(worker)
+    const proxy = createBatchProxy<IAPI>()
+
+    const result = await client.parallel(proxy.error('hello'))
+    close()
+
+    expect(result.length).toBe(1)
+    const err = result[0].unwrapErr()
     expect(err).toBeInstanceOf(Error)
     expect(err!.message).toMatch('hello')
   })
